6973174d44460b5c790d061f81a86a9c05a1125e
DevJihwan <jh552016@naver.com>
2022-08-04 08:58:36 +0900

    commit on main

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ ERC20이란 승인받은 개선 제안서 : EIP(Ethereum Imporvement Proposal) -
         * contacts : 스마트컨트랙트 작성 폴더
         * migrations : 배포 
     01. npm install openzeppelin-solidity --save (openzeppelin은 ERC20 토큰 발행 관련 오픈소스임)
+
+
+# 참고자료 
+ [openzeppelin-solidity rule.sol에 대한 설명] https://betterprogramming.pub/how-to-use-openzeppelins-new-accesscontrol-contract-5b49a4bcd160
--- contracts/SampleToken.sol
+++ contracts/SampleToken.sol
@@ -1,5 +1,18 @@
-pragma solidity ^0.4.24;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
 
-contract SampleToken {
-    
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+
+
+//openzeppelin-solidity/contract/token/ERC20에 있는 ERC20.sol 상속
+contract SampleToken is ERC20Detailed, ERC20, Ownable {
+    //생성자
+    constructor(string memory name, string memory symbol, uint8 decimals, uint256 totalSupply)
+    ERC20Detailed(name, symbol, decimals)
+
+    public{
+        _mint(owner(), totalSupply * 10**uint(decimals));
+    }
 }
