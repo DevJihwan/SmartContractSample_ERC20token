fdef78f0cf45c44165b95e7e23cb97c78e2ad892
DevJihwan <jh552016@naver.com>
2022-08-12 08:56:03 +0900

    commit on main

--- contracts/SampleToken.sol
+++ contracts/SampleToken.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/access/AccessControl.sol";
 
 //openzeppelin-solidity/contract/token/ERC20에 있는 ERC20.sol 상속
-contract SampleToken is ERC20{
-    
-    constructor() ERC20("Digital Warrior", "DWT"){
-        // mint 1000 token
+contract SampleToken is ERC20, AccessControl{
+
+    constructor(address ) ERC20("Digital Warrior", "DWT"){
         _mint(msg.sender, 10000 * (10 ** uint256(decimals())));
+
     }
 }
 
